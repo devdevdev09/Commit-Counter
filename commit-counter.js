eacaeec061ac3ce68d836c8ad4738775f24439b5
dev09 <devdevdevdevdev09@gmail.com>
2020-04-27 22:23:12 +0900

    카운트한것들 파일에 저장

--- commit-counter.js
+++ commit-counter.js
@@ -2,12 +2,16 @@ const cheerio = require("cheerio");
 const request = require("request");
 const moment  = require("moment");
 const jparser = require("./json-parser");
+const fs = require("fs");
 
 const GITHUB_URL  = "https://github.com/";
 
 const targets = (process.argv[2]) ? process.argv[2].split(",") 
                     : (jparser.getJsonData("github")) ? jparser.getJsonData("github").split(",") 
                         : "";
+
+const fileName = "file.txt";
+const fileExt = "utf8";
 
 let resultMsg = "";
 
@@ -21,9 +25,19 @@ const today = moment().day();
 
 const week = [];
 
+let resultmsg = "";
+
 for(let i = 0 ; i < 7; i++){
     week[6-i] = moment().add(- (i + today), 'days').format("YYYY-MM-DD"); 
 }
+
+let saveData = {
+    date : week[0] + " ~ " + week[6]
+}
+
+// saveData.test = {"test":"tst22"};
+// delete saveData.date
+// console.log(saveData);
 
 for(let i = 0 ; i < targets.length; i++){
     request(GITHUB_URL + targets[i], function(error, response, body){
@@ -45,10 +59,26 @@ for(let i = 0 ; i < targets.length; i++){
             }
             if(total > 0){
                 console.log("[" + targets[i] + "] : total commit count : " + total);
+                fileAppend(JSON.stringify({ 
+                        date : week[0] + " ~ " + week[6],
+                        name : targets[i],
+                        count : total
+                    }) + ","
+                );
             }else{
                 console.log("[" + targets[i] + "] : no commit :)");
             }
         }
+    });
+}
+
+const fileWrite = function(data){
+    fs.writeFileSync(fileName, data, fileExt);
+}
+
+const fileAppend = function(data){
+    fs.appendFile(fileName, data, function(){
+
     });
 }
 
